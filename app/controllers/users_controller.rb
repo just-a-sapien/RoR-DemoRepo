187aa788ca1ad3ae74eb110fd3557cf9cfe93d7b
just-a-sapien <vikash.sharma.nsit@gmail.com>
2025-05-12 02:56:26 +0530

    Add secure users#update with strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   def show
     render json: User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  private
+
+  # Only permit name and email to be updated
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
