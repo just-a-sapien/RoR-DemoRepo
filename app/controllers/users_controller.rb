196c35f343782ee6a85e72372c950d6907e9d2e7
just-a-sapien <vikash.sharma.nsit@gmail.com>
2025-05-14 16:31:32 +0530

    add secure create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def show
     render json: User.find(params[:id])
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :users, only: [ :index, :show ]
+  resources :users, only: [ :index, :show, :create ]
 end
